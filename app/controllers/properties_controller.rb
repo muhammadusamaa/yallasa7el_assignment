b72f4920b93dcea8999a3a93d73a4f846ef8fa50
Muhammed Usama <muhammed.osama26794@gmail.com>
2020-01-17 23:13:46 +0200

    added url user as a hyper link

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,19 +3,21 @@ class PropertiesController < ApplicationController
     
   end
 
-  def search  
+  def search
+    properties = Property.arel_table
+    destination = params[:property][:destination]
+    number_of_rooms = params[:property][:number_of_rooms]
+    if destination.blank? && number_of_rooms.blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    end
 
-  if params[:property][:destination].blank?  
-    redirect_to(root_path, alert: "Empty field!") and return  
-  else  
-    parameter = params[:property][:destination].capitalize      
-    @results = Property.where(destination: parameter)
-  end  
+    @results =     if destination.blank?
+      Property.where(number_of_rooms: number_of_rooms.to_i)
+    elsif number_of_rooms.blank?
+      Property.where("lower(destination) LIKE ?", "%#{destination}%")
+    else
+      Property.where("lower(destination) LIKE ? AND number_of_rooms = ?", "%#{destination}%", number_of_rooms.to_i)
+    end
+  end
 end
-  
-  #def search_by_destination
-    #Property.search do
-      #fulltext 'Gouna'
-    #end
-  #end
-end
+
